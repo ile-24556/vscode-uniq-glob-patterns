6b8ffd6988eb45a6317c549631c9fc9edae2ee2a
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2022-08-04 16:55:55 +0900

    Load external JSON

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,13 +1,13 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 import { uniq } from '../../uniq-glob-pattenrs';
+import { testData } from './test-data.json';
 
 suite('Extension Test Suite', () => {
     vscode.window.showInformationMessage('Start all tests.');
 
     test('Known values', () => {
-        const knownCases = require('./test-data.json');
-        for (let pair of knownCases) {
+        for (let pair of testData) {
             assert.deepStrictEqual(uniq(pair.input), pair.correctOutput);
         }
     });
