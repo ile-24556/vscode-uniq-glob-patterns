96001258926d898fce94edc57fec43fc972c730b
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2022-08-04 15:29:13 +0900

    Fix typo

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
-
-import { uniqGlobPattenrs } from './uniq-glob-pattenrs'
+import { uniqGlobPattenrs } from './uniq-glob-pattenrs';
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('uniq-glob-patterns.uniq', uniqGlobPattenrs);
