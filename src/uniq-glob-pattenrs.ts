14f233af8985eb787e4c58f9d9287a5c57c12b09
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2022-08-04 15:37:42 +0900

    Add functions

--- src/uniq-glob-pattenrs.ts
+++ src/uniq-glob-pattenrs.ts
@@ -1,1 +1,31 @@
-export function uniqGlobPattenrs() { }
+import * as vscode from 'vscode';
+
+export function uniqGlobPattenrs() {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return undefined;
+    }
+    const selection = editor.selection;
+    const range = normaliseRange(selection);
+    if (range.isEmpty) {
+        return undefined;
+    }
+    if (range.isSingleLine) {
+        return undefined;
+    }
+    return undefined;
+}
+
+function normaliseRange(range: vscode.Range) {
+    const normalizedRange = new vscode.Range(
+        range.start.line, 0, range.end.line + 1, 0);
+    return normalizedRange;
+}
+
+function loadLines(editor: vscode.TextEditor, range: vscode.Range) {
+    const text = editor.document.getText(range);
+    const splitters = /[\n\r]/;
+    const lines = text.split(splitters);
+    const nonemptyLines = lines.filter(word => word);
+    return nonemptyLines;
+}
