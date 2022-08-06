cd0e154bcd9e4d0a8a6fd0e1c19162536522a467
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2022-08-06 13:55:09 +0900

    Add test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -2,11 +2,18 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 import { uniq, convertGlobToRegex } from '../../uniq-glob-pattenrs';
 import { testData } from './uniq-cases.json';
+import * as convertCases from './pattern-convert-cases.json';
 
 suite('Extension Test Suite', () => {
     vscode.window.showInformationMessage('Start all tests.');
 
-    test('Known values', () => {
+    test('Test convertGlobToRegex() w/ Known values', () => {
+        for (let pair of convertCases) {
+            assert.deepStrictEqual(convertGlobToRegex(pair.input), pair.correctOutput);
+        }
+    });
+
+    test('Test uniq() w/ Known values', () => {
         for (let pair of testData) {
             assert.deepStrictEqual(uniq(pair.input), pair.correctOutput);
         }
