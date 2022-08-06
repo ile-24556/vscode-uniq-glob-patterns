518ea26b0491e75bd03d42f727e937c2b4b3b1b1
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2022-08-06 13:56:14 +0900

    Change data structure: Shallower

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,20 +1,20 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 import { uniq, convertGlobToRegex } from '../../uniq-glob-pattenrs';
-import { testData } from './uniq-cases.json';
-import * as convertCases from './pattern-convert-cases.json';
+import * as patternConvertCases from './pattern-convert-cases.json';
+import * as uniqCases from './uniq-cases.json';
 
 suite('Extension Test Suite', () => {
     vscode.window.showInformationMessage('Start all tests.');
 
     test('Test convertGlobToRegex() w/ Known values', () => {
-        for (let pair of convertCases) {
+        for (let pair of patternConvertCases) {
             assert.deepStrictEqual(convertGlobToRegex(pair.input), pair.correctOutput);
         }
     });
 
     test('Test uniq() w/ Known values', () => {
-        for (let pair of testData) {
+        for (let pair of uniqCases) {
             assert.deepStrictEqual(uniq(pair.input), pair.correctOutput);
         }
     });
