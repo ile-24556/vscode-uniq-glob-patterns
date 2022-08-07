1f60b09d102f24fe4f1a0e8a709c0f93e4d3127e
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2022-08-07 15:16:07 +0900

    Fix test to match implementation change

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -7,13 +7,14 @@ import * as uniqCases from './uniq-cases.json';
 suite('Extension Test Suite', () => {
     vscode.window.showInformationMessage('Start all tests.');
 
-    test('Test convertGlobToRegex() w/ Known values', () => {
+    test('Test convertGlobToRegex() w/ K`known values', () => {
         for (let pair of patternConvertCases) {
-            assert.strictEqual(translateGlobIntoRegex(pair.input), pair.correctOutput);
+            assert.strictEqual(translateGlobIntoRegex(pair.input).regex,
+                pair.correctOutput);
         }
     });
 
-    test('Test uniq() w/ Known values', () => {
+    test('Test uniq() w/ known values', () => {
         for (let pair of uniqCases) {
             assert.deepStrictEqual(uniq(pair.input), pair.correctOutput);
         }
